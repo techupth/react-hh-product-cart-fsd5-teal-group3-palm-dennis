c66f648a7053a4f977c4c7684ae8e12f9671c50f
Palm <palml1lax@gmail.com>
2023-12-22 13:18:16 +0700

    feat: Finish ex

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,133 @@
+import { useState } from "react";
 import "./App.css";
+import products from "./data/products";
 
 function App() {
+  const [productList, setProductList] = useState([...products]);
+  const [cartList, setCartList] = useState([]);
+  const [cartListID, setCartListID] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addToCart = (index) => {
+    if (!cartListID.includes(productList[index].id)) {
+      const newProductInCart = productList[index];
+      const newCartList = [...cartList];
+      newProductInCart.quantity = 1;
+      newCartList.push(newProductInCart);
+      //validate item in cartlist
+      setCartListID([...cartListID, newProductInCart.id]);
+      setCartList([...newCartList]);
+      setTotal(total + newProductInCart.price);
+    } else {
+      const targetProduct = products[index].id;
+      const newArr = cartList.map((item) => {
+        if (item.id === targetProduct) {
+          item.quantity += 1;
+          setTotal(total + item.price);
+          return item;
+        } else {
+          return item;
+        }
+      });
+      setCartList([...newArr]);
+    }
+  };
+
+  const deleteItem = (index) => {
+    setTotal(total - cartList[index].quantity * cartList[index].price);
+    const newArr = [...cartList];
+    newArr.splice(index, 1);
+    setCartList([...newArr]);
+    const newID = [...cartListID];
+    newID.splice(index, 1);
+    setCartListID([...newID]);
+  };
+
+  const addQuantity = (index) => {
+    setTotal(total + cartList[index].price);
+    const newArr = [...cartList];
+    newArr[index].quantity += 1;
+    setCartList([...newArr]);
+  };
+  const subtractQuantity = (index) => {
+    const newArr = [...cartList];
+    if (newArr[index].quantity > 0) {
+      setTotal(total - cartList[index].price);
+      newArr[index].quantity -= 1;
+      setCartList([...newArr]);
+    }
+    if (newArr[index].quantity === 0) {
+      newArr.splice(index, 1);
+      setCartList([...newArr]);
+      const newCartListID = [...cartListID];
+      newCartListID.splice(index, 1);
+      setCartListID(newCartListID);
+    }
+  };
   return (
     <div className="App">
       <section className="product-container">
         <h1 className="product-heading">Products</h1>
         <div className="product-list">
-          <div className="product">
-            <img
-              src="http://dummyimage.com/350x350.png/dddddd/000000"
-              alt="sample name"
-            />
-            <h2>Sample name</h2>
-            <p>Sample desc</p>
-            <button>Add to cart</button>
-          </div>
+          {productList.map((item, index) => {
+            return (
+              <div className="product" key={index}>
+                <img src={item.image} alt="sample name" />
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+                <button
+                  onClick={() => {
+                    addToCart(index);
+                  }}
+                >
+                  Add to cart
+                </button>
+              </div>
+            );
+          })}
         </div>
       </section>
       <hr />
 
       <section className="cart">
-        <h1 className="cart-heading">Cart (Total Price is x Baht)</h1>
+        <h1></h1>
+        <h1 className="cart-heading">Cart (Total Price is {total} Baht)</h1>
         <div className="cart-item-list">
-          <div className="cart-item">
-            <h1>Item name: Fond - Neutral</h1>
-            <h2>Price: 160 Baht</h2>
-            <h2>Quantity: 2</h2>
-            <button className="delete-button">x</button>
-            <div className="quantity-actions">
-              <button className="add-quantity">+</button>
-              <button className="subtract-quantity">-</button>
-            </div>
-          </div>
+          {cartList.map((item, i) => {
+            return (
+              <div className="cart-item" key={i}>
+                <h1>Item name: {item.name}</h1>
+                <h2>Price: {item.price} Baht</h2>
+                <h2>Quantity: {item.quantity}</h2>
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteItem(i);
+                  }}
+                >
+                  x
+                </button>
+                <div className="quantity-actions">
+                  <button
+                    className="add-quantity"
+                    onClick={() => {
+                      addQuantity(i);
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    className="subtract-quantity"
+                    onClick={() => {
+                      subtractQuantity(i);
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
